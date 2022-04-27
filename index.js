96184a6831713213cfb62944719db88345b5f3c2
abukawsar47 <abukawsar47ak@gmail.com>
2022-04-28 01:37:04 +0600

    solving

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.li9ve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         await client.connect();
         const productCollection = client.db('emaJohn').collection('product');
-
+        console.log('All route should be work');
         app.get('/product', async (req, res) => {
             const page = parseInt(req.query.page);
             const size = parseInt(req.query.size);

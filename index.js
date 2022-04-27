dd4417333527a0109507273c0a41a640ecbf0814
abukawsar47 <abukawsar47ak@gmail.com>
2022-04-27 23:25:42 +0600

    heroku testing

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.li9ve.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
